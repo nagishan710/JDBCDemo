6b1016536dfec3efe59508dc3e9b0bf49726d96e
Ishan Nag <nagishan710@gmail.com>
2023-06-15 23:19:56 +0530

    final commit

---
+++ src/com/movie/LesserProductionCostException.java
@@ -0,0 +1,14 @@
+package com.movie;
+
+public class LesserProductionCostException extends RuntimeException {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LesserProductionCostException(String message) {
+        super(message);
+    }
+}
+
+
---
+++ src/com/movie/ShorterMovieNameException.java
@@ -0,0 +1,14 @@
+package com.movie;
+
+public class ShorterMovieNameException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ShorterMovieNameException(String message) {
+        super(message);
+    }
+}
+
+
